46df0ef5b69f94f1d534a6d9400fd1b661ca8bc2
antoniocarlosvr <antonio.junior@vrsoft.com.br>
2024-07-08 14:58:33 -0300

    08/07/2024

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoComponent } from './produto/produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: ProdutoComponent },
+  {path : 'produto', component: ProdutoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
