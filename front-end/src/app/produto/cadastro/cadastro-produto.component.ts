06d896449bc904c911b4adf8a5fd8a657bf0dcd5
antoniocarlosvr <antonio.junior@vrsoft.com.br>
2024-07-08 21:11:56 -0300

    Criado layout de consulta e cadastro

---
+++ front-end/src/app/produto/cadastro/cadastro-produto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro-produto.component.html',
+  styleUrls: ['./cadastro-produto.component.css']
+})
+export class CadastroProdutoComponent {
+
+}
