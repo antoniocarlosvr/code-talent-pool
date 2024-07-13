9259721236d2d30840b08944713400c7859d6027
antoniocarlosvr <antonio.junior@vrsoft.com.br>
2024-07-13 15:26:55 -0300

    Iniciando conexão back to front

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,9 +3,16 @@ import { ProdutoModule } from './modules/produto/produto.module';
 import { LojaModule } from './modules/loja/loja.module';
 import { ProdutoLojaModule } from './modules/produtoloja/produtoloja.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, ProdutoModule, LojaModule, ProdutoLojaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ProdutoModule,
+    LojaModule,
+    ProdutoLojaModule,
+  ],
   controllers: [],
   providers: [],
 })
